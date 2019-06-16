978fb171f2e15b93a1c947694e4441d50249d16c
Timur <dkatimullin@gmail.com>
2019-06-16 17:15:10 +0400

    Added AuthorSubscriber

---
+++ src/EventListener/AuthorSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\EventListener;
+
+use App\Helper\AuthorInterface;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\EventDispatcher\Tests\Debug\EventSubscriber;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class AuthorSubscriber extends EventSubscriber
+{
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return ['prePersist'];
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if ($entity instanceof AuthorInterface) {
+            if ($this->tokenStorage->getToken()) {
+                if ($this->tokenStorage->getToken()->getUser()) {
+                    if (!$entity->getAuthor()) {
+                        $entity->setAuthor($this->tokenStorage->getToken()->getUser());
+                    }
+                }
+            }
+        }
+    }
+}
